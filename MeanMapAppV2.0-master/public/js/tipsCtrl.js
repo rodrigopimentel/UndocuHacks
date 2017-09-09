5777f655d870462f941a89d6d1f2605d3ce63e99
Gerard O'Neill <grardb@gmail.com>
2017-09-09 15:46:51 -0700

    Tips page placeholder

---
+++ MeanMapAppV2.0-master/public/js/tipsCtrl.js
@@ -0,0 +1,7 @@
+var tipsCtrl = angular.module('tipsCtrl', []);
+tipsCtrl.controller('tipsCtrl', function($scope) {
+    // // Sets the isActive value based on the current URL location
+    // $scope.isActive = function (viewLocation) {
+    //     return viewLocation === $location.path();
+    // };
+});
